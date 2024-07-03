1eeaa806c5e7cbbac6cf8790b06ca47cd401eabd
Minhaz Jisun <minhaz.jisun@gmail.com>
2024-07-03 10:48:43 +0600

    Infra creation

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const securityGroup = new aws.ec2.SecurityGroup("web-secgrp", {
 // Create EC2 Instances
 const size = "t3.small"; // Change as needed
 
-const ami = "ami-04a81a99f5ec58529";
+const ami = "ami-060e277c0d4cce553";
 
 
 new aws.ec2.Instance("express-redis-mysql-server", {
